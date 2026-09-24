93fa355fca6268e73a56646467d5f0ba4d621c56
steyyrddsf <youmatisagnon@gmail.com>
2026-09-24 11:32:23 -0300

    Add MainActivity for displaying screen temperature

---
+++ app/src/main/java/com/exemple/tempsecran/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.exemple.tempsecran
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF121212)),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("Temps Écran", color = Color.White, fontSize = 28.sp)
+            }
+        }
+    }
+}
